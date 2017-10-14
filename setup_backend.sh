13b95e56ec9617d10195dd153bffa2be44a579ce
Nic Cheneweth <nchenewe@thoughtworks.com>
2017-10-14 18:42:38 -0500

    update config

--- setup_backend.sh
+++ setup_backend.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 cat <<EOF > backend.conf
-key=bootstrap-$1-roles/auth.tfstate
-bucket=feedyard-$1-local
-region=$2
-profile=bootstrap-$1
+key="bootstrap-$1-roles/auth.tfstate"
+bucket="feedyard-$1-local"
+region="$2"
+profile="bootstrap-$1"
 EOF
