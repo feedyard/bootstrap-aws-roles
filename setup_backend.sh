39d64a73b86aa46e15837e8957dd97a000b1a4be
ncheneweth <nchenewe@thoughtworks.com>
2018-01-05 18:13:14 -0600

    updated bucket name

--- setup_backend.sh
+++ setup_backend.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 cat <<EOF > backend.conf
-key="bootstrap-$1-roles/auth.tfstate"
-bucket="feedyard-$1-local"
+key="feedyard-$1-roles/auth.tfstate"
+bucket="feedyard-$1-state"
 region="$2"
 profile="default"
 EOF
